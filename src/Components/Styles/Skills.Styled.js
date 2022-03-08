0eee4837f4d194080afcb144841381a86c79d85f
Kevin-Laptop <alphalop42@gmail.com>
2022-03-07 16:56:48 -0800

    Changing skill set list into it's own file and mapping the array into Skills componenet

--- src/Components/Styles/Skills.Styled.js
+++ src/Components/Styles/Skills.Styled.js
@@ -10,9 +10,9 @@ export const StyledSkills = styled.section`
   padding: 30px;
   flex-wrap: wrap;
   }
-  .box {
+  .skill-element {
     width: 125px;
-    height: 125px;
+    //height: 125px;
     //background-image: linear-gradient(-90deg, rgba(49, 207, 171, 0.5) 0%, #50E3C2 100%);
       svg {
         //fill: url(#Gradient);
@@ -21,10 +21,9 @@ export const StyledSkills = styled.section`
         //background-image: linear-gradient(to top, #5ee7df 0%, #b490ca 100%);
       }
       h3 {
-        font-size: var(--font-xl);
+        font-size: var(--font-base);
         text-align: center;
       }
-
   }
   
 `
