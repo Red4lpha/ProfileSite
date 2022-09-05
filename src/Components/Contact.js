a3fca140a84cf41a84f9a0ffa6895241ad3cd5b2
Kevin <alphalop42@gmail.com>
2022-09-05 00:56:56 -0700

    Added the contact section along with desktop layout and styling

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,11 +1,57 @@
-import React from 'react'
+import { StyledContact } from './Styles/Contact.styled';
+import { StyledContainer } from './Styles/Container.Styled';
+import { StyledBorder } from './Styles/BorderWrapper.Styled';
+import { AiOutlineMail, AiOutlineLinkedin, AiOutlineAim} from "react-icons/ai";
+import { BsArrowRightCircle } from "react-icons/bs";
 
 const Contact = () => {
-    return (
-        <div>
-            contact
-        </div>
-    )
+  return (
+    <StyledContact>
+      <StyledContainer>
+          <StyledBorder>
+          <span className='title'><h2>CONTACT ME</h2></span>
+          <div className='contact-wrapper'>
+            <section className='contact-form'>
+              <h3>Send me a message</h3>
+              <div className='contact-form-top'>
+                <div>
+                  <label for='name'>Name</label>
+                  <input type='text' for='name' id='name' placeholder='your name...'></input>
+                </div>
+                <div>
+                  <label for='email'>Email</label>
+                  <input type='email' for='email' id='email' placeholder='your email...'></input>
+                </div>
+              </div>
+              <label for='msg'>Message</label>
+              <textarea type='text' for='msg' id='msg' placeholder='your message...'></textarea>
+              <button className='form-button button'>
+                <span>Send</span>
+                <BsArrowRightCircle /> 
+              </button>
+            </section>
+            <section className='contact-info'>
+              <img src='./Media/TestPhoto.jpg' alt='Portrait of Kevin' /> 
+              <ul>
+                <li>
+                  <AiOutlineMail />
+                  myemail@email.com
+                </li>
+                <li>
+                  <AiOutlineLinkedin />
+                  Linkedin URL
+                  </li>
+                <li>
+                  <AiOutlineAim />
+                  San Diego
+                </li>
+              </ul>
+            </section>
+          </div>
+          </StyledBorder>
+      </StyledContainer>
+    </StyledContact>
+  )
 }
 
 export default Contact
---
+++ src/Components/Styles/Contact.styled.js
@@ -0,0 +1,123 @@
+import styled from "styled-components";
+
+export const StyledContact = styled.section`
+  .contact-wrapper {
+  display: flex;
+  flex-direction: row;
+  //gap: 40px;
+  justify-content: center;
+  align-items: flex-start;
+  padding: 30px;
+  font-size: var(--font-base);
+  }
+    h3 {
+      padding-bottom: 20px;
+    }
+    label, input {
+      display: block;
+    }
+    input, textarea {
+      background-color: rgba(255,255,255,.1);
+      color: rgba(255,255,255,.7);
+      background-clip: padding-box;
+      border-color: rgba(255,255,255,.08);
+      border-radius: 5px;
+      padding: 5px;
+      width: 100%;
+    }
+    label {
+      padding-bottom: 5px;
+    }
+    textarea:focus, input:focus{
+      outline: 0.5px solid ${({ theme }) => theme.dark.hoverBlue}
+    }
+    //Contact form section ----------------
+    .contact-form {
+      flex-basis: 75%;
+      border-right: 0.5px solid ${({ theme }) => theme.dark.border};
+      padding-right: 20px;
+    } 
+    .contact-form-top {
+      display: flex;
+      gap: 10px;
+      padding-bottom: 20px;
+
+      & > div {
+        flex-basis: 50%;
+      }
+    }
+
+    #msg {
+      width: 100%;
+      height: 200px;
+    } 
+    
+    .form-button {
+      margin-top: 10px;
+      padding: 10px 55px;
+      //background-color: ${({ theme }) => theme.dark.bodyFont};
+      //background-color: ${({ theme }) => theme.dark.hoverBlue};
+      //border: 1px solid ${({ theme }) => theme.dark.hoverBlue};
+      border: 1px solid ${({ theme }) => theme.dark.bodyFont};
+      background-color: inherit;
+      border-radius: 5px;
+      color: white;
+      outline: none;
+      font-weight: bolder;
+      font-size: var(--font-base);
+      text-align: center;
+      display: flex;
+      align-items: center;
+      gap: 10px;
+
+      svg {
+        stroke-width: 1px;
+      }
+    }
+    .form-button:hover {
+      //background-color: ${({ theme }) => theme.dark.bodyFont};
+      border: 1px solid ${({ theme }) => theme.dark.hoverBlue}; 
+
+      svg {
+        color: ${({ theme }) => theme.dark.hoverBlue}; 
+      }
+    }
+
+
+    //Contact Info section ---------------------
+    .contact-info {
+      flex-basis: 25%;
+      padding-left: 20px;
+      align-self: center;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      gap: 20px;
+      font-size: .9em;
+
+      img {
+        border-radius: 50%;
+        width: 175px;
+        height: 175px;
+        object-fit: cover; 
+      }
+      ul {
+        width: 100%;
+      }
+
+      ul > *:not(:last-child) {
+        border-bottom: 0.5px solid ${({ theme }) => theme.dark.border};
+      }
+      li {
+        padding: 7px;
+        color: ${({ theme }) => theme.dark.paragraphFont};
+      }
+      svg {
+        margin-right: 10px;
+        fill: ${({ theme }) => theme.dark.hoverBlue};
+
+      }
+    }
+
+`
